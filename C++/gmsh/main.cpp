84bfa04ef6d20dd3688adeb612611477c464fb30
Patrick <patrickdiehl@lsu.edu>
2023-04-06 13:01:50 -0500

    Add files

---
+++ C++/gmsh/main.cpp
@@ -0,0 +1,12 @@
+#include <gmsh.h>
+#include <blaze/Blaze.h>
+
+void readGmsh(const std::string filename, blaze::DynamicMatrix<double> gcoord, blaze::DynamicMatrix<size_t> nodes, size_t dim){} 
+
+
+int main(void){
+
+
+    return EXIT_SUCCESS;
+
+}
